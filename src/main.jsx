f3f8095f106407295c44bb5ebd5939925e4d956f
Jose Thankachan(UST <225698@ust.com>
2026-06-03 23:32:43 +0530

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import CADOverlayApp from './CADOverlayApp'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <CADOverlayApp />
+  </React.StrictMode>
+)
